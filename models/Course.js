899ba1bd0aacad6337c08d4b0120b5cf87a4370f
Nomi77 <naeemtahir775@gmail.com>
2020-10-22 19:24:55 +0500

    Averge cost calculation for a bootcamp functionality added

--- models/Course.js
+++ models/Course.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Bootcamp = require('./Bootcamp');
 
 const CourseSchema = new mongoose.Schema(
   {
@@ -50,6 +51,45 @@ const CourseSchema = new mongoose.Schema(
   }
 );
 
+// Static method to calculate average cost for a bootcamp
+CourseSchema.statics.calcAverageCost = async function (bootcampId) {
+  const cost = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: { $avg: '$tuition' },
+      },
+    },
+  ]);
+  console.log(cost);
+  if (cost.length > 0) {
+    await Bootcamp.findByIdAndUpdate(bootcampId, {
+      averageCost: Math.round(cost[0].averageCost / 10) * 10,
+    });
+  }
+};
+
+// Middleware to call the calculateAverage method
+CourseSchema.post('save', function () {
+  this.constructor.calcAverageCost(this.bootcamp);
+});
+
+// middleware to get id of the course which is being updated or deleted
+CourseSchema.pre(/^findOneAnd/, async function (next) {
+  this.course = await this.findOne();
+  // console.log(this.course);
+  next();
+});
+
+// Middleware to update the average Cost of the course after deleting or updating a course
+CourseSchema.post(/^findOneAnd/, async function () {
+  this.course.constructor.calcAverageCost(this.course.bootcamp._id);
+});
+
+//Middleware to Populate the bootcamps and users data for a course
 CourseSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'bootcamp',
